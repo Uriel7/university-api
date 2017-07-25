28c3a0d18dfc5006150718edcc99bc83584d4c13
Uriel <j.urielsz@gmail.com>
2017-07-25 15:37:50 -0500

    Add Relationships and dependent destroy

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,4 @@
 class Score < ApplicationRecord
-  belongs_to :student_id
-  belongs_to :subject_id
+  belongs_to :student, dependent: :destroy
+  belongs_to :subject
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ApplicationRecord
+  has_many :scores, dependent: :destroy
+
+  validates_presence_of :name, :surname, :birthdate
 end
